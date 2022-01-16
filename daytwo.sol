51276be02527d5bd1e49acb4b165888dafa83a4f
Prathamesh madniwale <61145586+prathamesh2525@users.noreply.github.com>
2022-01-16 23:01:21 +0530

    created daytwo.sol

---
+++ daytwo.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Identity{
+    string name;
+    uint age;
+
+    constructor() public{
+        name = "Prathamesh";
+        age = 21;
+    }
+
+    function getName() view public returns(string memory){
+        return name;
+    } 
+
+    function getAge() view public returns(uint){
+        return age;
+    }
+
+    function setAge() public{
+        age=age+1;
+    }
+}
